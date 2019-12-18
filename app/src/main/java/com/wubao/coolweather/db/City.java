e2b2791ef11cd3e538e68ce847f8ec4dacc89f73
zhouhua_wu <1610291@gmail.com>
2019-12-18 22:47:02 +0800

    加入请求

--- app/src/main/java/com/wubao/coolweather/db/City.java
+++ app/src/main/java/com/wubao/coolweather/db/City.java
@@ -6,6 +6,7 @@ public class City extends DataSupport {
     private int id;
     private String cityName;
     private int cityCode;
+    private int provinceId;
 
     public int getId() {
         return id;
@@ -29,5 +30,13 @@ public class City extends DataSupport {
 
     public void setCityCode(int cityCode) {
         this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
     }
 }
